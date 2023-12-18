4cb06a27a1b796afb85488e6bfc639564cad7e41
Corentin Deleuse <dlse.corentin@gmail.com>
2023-12-19 00:41:18 +0100

    feat: update tailwind config

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,19 +2,24 @@ import { Link } from "react-router-dom";
 
 function Sidebar() {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="students">Students</Link>
-                </li>
-                <li>
-                    <Link to="classes">Classes</Link>
-                </li>
-            </ul>
-        </nav>
+        <div>
+            <div>
+                <Link className="font-bold text-[40px]" to="/">Spher<span className="bg-gradient-to-b from-primary to-[#F1B575] inline-block text-transparent bg-clip-text">IIM</span></Link>
+            </div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="students">Students</Link>
+                    </li>
+                    <li>
+                        <Link to="classes">Classes</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#F07D00',
+        'secondary': '#F07D00',
+        'dark': '#636466',
+        'light': '#F3F3F3',
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
