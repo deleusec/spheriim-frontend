e1242e1db58d4f0692f42314b3904a9df0306d4d
Corentin Deleuse <dlse.corentin@gmail.com>
2023-12-19 14:14:32 +0100

    update sidebar content and style
    #3

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,13 +1,16 @@
 import { Link, useMatch } from "react-router-dom";
 import SidebarButton from "./SidebarButton";
-import { HomeIcon, AcademicCapIcon, UsersIcon } from '@heroicons/react/24/outline';
+import { HomeIcon, AcademicCapIcon, UsersIcon, BriefcaseIcon, Cog6ToothIcon, ArrowRightStartOnRectangleIcon } from '@heroicons/react/24/outline';
 
 function Sidebar() {
     const isHome = useMatch('/') ? true : false;
     const isStudents = useMatch('/students') ? true : false;
     const isClasses = useMatch('/classes') ? true : false;
+    const isTeachers = useMatch('/teachers') ? true : false;
+    const isSettings = useMatch('/settings') ? true : false;
+    const isLogin = useMatch('/login') ? true : false;
 
-    
+
     return (
         <div className="flex flex-col items-center h-[100vh] gap-5 p-10">
             <div className="mb-10">
@@ -17,7 +20,7 @@ function Sidebar() {
                 <ul>
                     <li>
                         <Link to="/">
-                            <SidebarButton text="Home" active={isHome} >
+                            <SidebarButton text="Accueil" active={isHome} >
                                 <HomeIcon className="w-6 h-6" />
                             </SidebarButton>
                         </Link>
@@ -36,8 +39,31 @@ function Sidebar() {
                             </SidebarButton>
                         </Link>
                     </li>
+                    <li>
+                        <Link to="teachers">
+                            <SidebarButton text="Professeurs" active={isTeachers}>
+                                <BriefcaseIcon className="w-6 h-6" />
+                            </SidebarButton>
+                        </Link>
+                    </li>
+
                 </ul>
             </nav>
+
+            <div className="flex-1"></div>
+
+            <div>
+                <Link to="settings">
+                    <SidebarButton text="Paramètres" active={isSettings}>
+                        <Cog6ToothIcon className="w-6 h-6" />
+                    </SidebarButton>
+                </Link>
+                <Link to="login">
+                    <SidebarButton text="Déconnexion" active={isLogin}>
+                        <ArrowRightStartOnRectangleIcon className="w-6 h-6" />
+                    </SidebarButton>
+                </Link>
+            </div>
         </div>
     );
 }
---
+++ src/pages/authentication/Login.tsx
@@ -0,0 +1,19 @@
+function Login() {
+    return ( 
+        <div>
+            <h1>Login</h1>
+
+            <form>
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" />
+
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" />
+
+                <button type="submit">Login</button>
+            </form>
+        </div>
+     );
+}
+
+export default Login;
