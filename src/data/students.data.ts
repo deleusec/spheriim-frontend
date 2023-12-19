363d9e6bf869f489c32230b4fc04ddd914c3f8aa
youssef Charafeddine <yosuke2017@hotmail.com>
2023-12-19 15:42:12 +0100

    Create Student List

---
+++ src/data/students.data.ts
@@ -0,0 +1,85 @@
+import CamilleImage from '../../public/images/Camille.png'
+import YasmineImage from '../../public/images/Yasmine.png'
+import ClaudiaImage from '../../public/images/Claudia.png'
+import MathieuImage from '../../public/images/Mathieu.png'
+import { Student } from '../types/Students.type'
+
+export const students: Student[] = [
+    {
+      profilePic: CamilleImage,
+      name: 'Camille DESMOULINS',
+      class: 'IIM A4 FL',
+      email: 'camilledesmoulins@edu.devinci.fr',
+      startYear: 'A2(2021-2022)',
+    },
+    {
+      profilePic:YasmineImage,
+      name: 'Yasmine ROSA',
+      class: 'IIM A4 FL',
+      email: 'yasminerosa@edu.devinci.fr',
+      startYear: 'A2(2021-2022)',
+    },
+    {
+      profilePic: ClaudiaImage,
+      name: 'Claudia AZUR',
+      class: 'IIM A4 FL',
+      email: 'claudiaazur@edu.devinci.fr',
+      startYear: 'A2(2021-2022)',
+    },
+    {
+      profilePic: MathieuImage,
+      name: 'Mathieu ROJO',
+      class: 'IIM A4 FL',
+      email: 'mathieurojo@edu.devinci.fr',
+      startYear: 'A2(2021-2022)',
+    },
+    {
+      profilePic: MathieuImage,
+      name: 'Mathieu ROJO',
+      class: 'IIM A4 FL',
+      email: 'mathieurojo@edu.devinci.fr',
+      startYear: 'A2(2021-2022)',
+    },
+    {
+      profilePic: MathieuImage,
+      name: 'Mathieu ROJO',
+      class: 'IIM A4 FL',
+      email: 'mathieurojo@edu.devinci.fr',
+      startYear: 'A2(2021-2022)',
+    },
+    {
+      profilePic: MathieuImage,
+      name: 'Mathieu ROJO',
+      class: 'IIM A4 FL',
+      email: 'mathieurojo@edu.devinci.fr',
+      startYear: 'A2(2021-2022)',
+    },
+    {
+      profilePic: MathieuImage,
+      name: 'Mathieu ROJO',
+      class: 'IIM A4 FL',
+      email: 'mathieurojo@edu.devinci.fr',
+      startYear: 'A2(2021-2022)',
+    },
+    {
+      profilePic: MathieuImage,
+      name: 'Mathieu ROJO',
+      class: 'IIM A4 FL',
+      email: 'mathieurojo@edu.devinci.fr',
+      startYear: 'A2(2021-2022)',
+    },
+    {
+      profilePic: MathieuImage,
+      name: 'Mathieu ROJO',
+      class: 'IIM A4 FL',
+      email: 'mathieurojo@edu.devinci.fr',
+      startYear: 'A2(2021-2022)',
+    },
+    {
+      profilePic: MathieuImage,
+      name: 'Mathieu ROJO',
+      class: 'IIM A4 FL',
+      email: 'mathieurojo@edu.devinci.fr',
+      startYear: 'A2(2021-2022)',
+    },
+  ]
