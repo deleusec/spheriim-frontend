b4d7de1b28a96ada0acc8889fd4ac90882c951b6
Corentin Deleuse <dlse.corentin@gmail.com>
2023-12-20 14:29:35 +0100

    fix: component typage

--- src/components/HeaderCards.tsx
+++ src/components/HeaderCards.tsx
@@ -1,19 +1,17 @@
-import React from 'react';
+function HeaderCards(props: { svg: string, title: string }) {
 
-class HeaderCards extends React.Component {
-    render() {
-        const {svg, title} = this.props;
+    const { svg, title } = props;
 
-        return (
-            <div className="flex items-center gap-4 mb-2">
-                <div className='bg-primary h-9 w-9 rounded-xl flex justify-center items-center'>                
-                    <img src={svg} alt="picto" className='h-[18px] w-[18px]'/>
-                </div>
-                <h3 className="text-xl font-medium">{title}</h3>
+    return (
+        <div className="flex items-center gap-4 mb-2">
+            <div className='bg-primary h-9 w-9 rounded-xl flex justify-center items-center'>
+                <img src={svg} alt="picto" className='h-[18px] w-[18px]' />
             </div>
-        );
-    }
+            <h3 className="text-xl font-medium">{title}</h3>
+        </div>
+    );
 }
+
 
 
 export default HeaderCards;
--- src/components/LinksList.tsx
+++ src/components/LinksList.tsx
@@ -1,33 +1,32 @@
-import React from 'react';
 import HeaderCards from './HeaderCards';
 import LinkIcon from '../assets/icons/link.svg';
 
-class LinksList extends React.Component {
-    render() {
-        const { github, linkedin, portfolio, cv } = this.props;
+function LinksList(props: { github: string, linkedin: string, portfolio: string, cv?: string }) {
 
-        return (
-            <div className=" p-[16px] w-full rounded-xl shadow-cards flex flex-col gap-4 bg-white">
-                <HeaderCards svg={LinkIcon} title="Liens"/>
-                <div>
-                    <ul className="flex flex-col gap-4 text-sm">
-                        <li className="text-primary">
-                            Github : <a href={`https://github/${github}`} target="_blank" className="text-black border-b-[1px] border-black mx-2">{github}</a>
-                        </li>
-                        <li className="text-primary">
-                            Linkedin : <a href={linkedin} target="_blank" className="text-black border-b-[1px] border-black mx-2">{linkedin}</a>
-                        </li>
-                        <li className="text-primary">
-                            Portfolio : <a href={portfolio} target="_blank" className="text-black border-b-[1px] border-black mx-2">{portfolio}</a>
-                        </li>
-                        <li className="text-primary">
-                            CV : <a href={cv} target="_blank" className="text-black border-b-[1px] border-black mx-2">Cliquer ici</a>
-                        </li>
-                    </ul>
-                </div>
+    const { github, linkedin, portfolio, cv } = props;
+
+    return (
+        <div className=" p-[16px] w-full rounded-xl shadow-cards flex flex-col gap-4 bg-white">
+            <HeaderCards svg={LinkIcon} title="Liens" />
+            <div>
+                <ul className="flex flex-col gap-4 text-sm">
+                    <li className="text-primary">
+                        Github : <a href={`https://github/${github}`} target="_blank" className="text-black border-b-[1px] border-black mx-2">{github}</a>
+                    </li>
+                    <li className="text-primary">
+                        Linkedin : <a href={linkedin} target="_blank" className="text-black border-b-[1px] border-black mx-2">{linkedin}</a>
+                    </li>
+                    <li className="text-primary">
+                        Portfolio : <a href={portfolio} target="_blank" className="text-black border-b-[1px] border-black mx-2">{portfolio}</a>
+                    </li>
+                    <li className="text-primary">
+                        CV : <a href={cv} target="_blank" className="text-black border-b-[1px] border-black mx-2">Cliquer ici</a>
+                    </li>
+                </ul>
             </div>
-        )
-    }
+        </div>
+    )
+
 }
 
 export default LinksList;
