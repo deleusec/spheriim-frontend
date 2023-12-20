fff63f00e74054f5caddec172438fc31d12f308e
Corentin Deleuse <dlse.corentin@gmail.com>
2023-12-20 09:50:16 +0100

    feat(sidebar): add the toggle sidebar

--- src/components/SidebarButton.tsx
+++ src/components/SidebarButton.tsx
@@ -1,6 +1,7 @@
 interface SidebarButtonProps {
     children?: React.ReactNode;
     active?: boolean | undefined;
+    sidebarIsOpen?: boolean;
     text: string;
     theme: 'light' | 'dark';
     onClick?: () => void;
@@ -8,17 +9,18 @@ interface SidebarButtonProps {
 
 SidebarButton.defaultProps = {
     active: false,
+    sidebarIsOpen: true,
     text: 'Click Me',
     theme: 'light'
 }
 
 function SidebarButton(props: SidebarButtonProps) {
-    return ( 
-        <button className={`flex items-center gap-4 min-w-[250px] py-3 px-4 rounded-lg text-sm ${props.active && 'bg-primary text-white '}`} onClick={props.onClick}>
-            { props.children }
-            <span>{ props.text }</span>
+    return (
+        <button className={`flex items-center gap-4 py-3 px-4 rounded-lg text-sm ${props.active && 'bg-primary text-white '} ${props.sidebarIsOpen && "min-w-[250px]"}`} onClick={props.onClick}>
+            {props.children}
+            {props.sidebarIsOpen && <span>{props.text}</span>}
         </button>
-     );
+    );
 }
 
 export default SidebarButton;
