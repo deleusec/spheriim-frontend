6ce1fd33d6b584a8bf279439dcb4289330a09f0d
YesIDo49 <mehdialsidcheikh@gmail.com>
2023-12-20 11:31:19 +0100

    add props to reuse student components

--- src/components/StudentInfo.tsx
+++ src/components/StudentInfo.tsx
@@ -1,21 +1,32 @@
-function StudentInfo() {
-    return (
-        <section className="w-full flex justify-center items-center gap-12 p-6 mb-8 bg-white">
-            <div className="w-1/3 md:w-1/5 max-w-[200px]">
-                <img className="w-full h-full object-contain rounded-full" src="https://varbai.com/wp-content/uploads/2019/02/thispersondoesnotexis.jpg" alt="student picture"/>
-            </div>
-            <div>
-                <h2 className="mb-6 text-xl lg:text-2xl">Camille <span className="uppercase">Desmoulins</span></h2>
-                <ul className="gap-4">
-                    <li className="mb-2"><span className="text-primary font-medium">Mail : </span> <a className="underline" href="mailto:camilledesmoulins@edu.devinci.fr">camilledesmoulins@edu.devinci.fr</a></li>
-                    <li className="mb-2"><span className="text-primary font-medium">Axe :</span> Fullstack</li>
-                    <li className="mb-2"><span className="text-primary font-medium">Année : </span> A4</li>
-                    <li className="mb-2"><span className="text-primary font-medium">Début à l'IIM : </span> CDI A2 (2021 - 2022)</li>
-                    <li className="mb-2"><span className="text-primary font-medium">Alternant(e) : </span> Développeuse Fullstack - Crédit Agricole</li>
-                </ul>
-            </div>
-        </section>
-    );
+import React from "react";
+
+class StudentInfo extends React.Component {
+    render() {
+        const { firstname, name, mail, axe, grade, startYear, jobPosition, job, company } = this.props;
+
+        return (
+            <section className="w-full flex justify-center items-center gap-12 p-6 mb-8 bg-white">
+                <div className="w-1/3 md:w-1/5 max-w-[200px]">
+                    <img className="w-full h-full object-contain rounded-full" src="https://varbai.com/wp-content/uploads/2019/02/thispersondoesnotexis.jpg" alt="student picture"/>
+                </div>
+                <div>
+                    <h2 className="mb-6 text-xl lg:text-2xl">{firstname} <span className="uppercase">{name}</span></h2>
+                    <ul className="gap-4">
+                        <li className="mb-2"><span className="text-primary font-medium">Mail : </span> <a className="underline" href={`mailto:${mail}`}>{mail}</a></li>
+                        <li className="mb-2"><span className="text-primary font-medium">Axe :</span> {axe}</li>
+                        <li className="mb-2"><span className="text-primary font-medium">Année : </span> {grade}</li>
+                        <li className="mb-2"><span className="text-primary font-medium">Début à l'IIM : </span> {startYear}</li>
+                        {job && jobPosition && company && (
+                            <li className="mb-2">
+                                <span className="text-primary font-medium">{jobPosition} : </span>
+                                {job} - {company}
+                            </li>
+                        )}
+                    </ul>
+                </div>
+            </section>
+        );
+    }
 }
 
 export default StudentInfo;
--- src/pages/Student.tsx
+++ src/pages/Student.tsx
@@ -9,11 +9,11 @@ import KeyPoints from "../components/KeyPoints";
 
 function Student() {
     return (
-        <>
-            <StudentInfo/>
+        <section className="flex flex-col w-full">
+            <StudentInfo firstname="Camille" name="Desmoulins" mail="camilledesmoulins@edu.devinci.fr" axe="Fullstack" grade="A4" startYear="CDI A2 (2021 - 2022)" job="Développeuse Fullstack" jobPosition="Alternante" company="Crédit Agricole"/>
             <div className="grid grid-cols-9 auto-rows-auto gap-5 px-5">
                 <div className="flex col-start-1 col-span-4 row-start-1 row-span-1 h-full w-full">
-                    <LinksList/>
+                    <LinksList github="CamilleDev" linkedin="www.linkedin.com/in/camille" portfolio="https://portfolio-camille"/>
                 </div>
                 <div className="flex col-start-5 col-span-6 row-start-1 row-span-1 h-full w-full">
                     <StudentBio bio="Souhaite devenir développeuse Front-end ou chef de projet et travailler à l’étranger."/>
