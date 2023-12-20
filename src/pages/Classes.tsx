875bd82962e4890d8a6f6322a97d652245e3ac22
walltch <walltch@github.com>
2023-12-20 09:58:52 +0100

    first-commit

--- src/pages/Classes.tsx
+++ src/pages/Classes.tsx
@@ -1,7 +1,26 @@
 function Classes() {
     return ( 
         <>
-            <h1>Classes</h1>
+        <div className="box w-100 flex items-center h-full justify-center">
+            <div className="col-f flex column shadow-xl rounded-2xl">
+                <ul className="m-10 border-2">
+                    <li className="py-6 pr-16 pl-2 border-b-2"><a href="">Communication digitale et e-business</a></li>
+                    <li className="py-6 pr-16 pl-2 border-b-2"><a href="">Coding & Digital Innovation</a></li>
+                    <li className="py-6 pr-16 pl-2 border-b-2"><a href="">Animation 3D</a></li>
+                    <li className="py-6 pr-16 pl-2 border-b-2"><a href="">Jeux Vidéo</a></li>
+                    <li className="py-6 pr-16 pl-2 border-b-2"><a href=""></a>Création et design</li>
+                    <li className="py-6 pr-16 pl-2 border-b-2"><a href="">Audiovisuel</a></li>
+                    <li className="py-6 pr-16 pl-2"><a href="">IIM Summer School</a></li>
+                </ul>
+                <ul className="p-20">
+                    <li className="p-5 list-disc max-w-xs hover:bg-sky-700 "><a href=""></a>Mastère Stratégie e-business</li>
+                    <li className="p-5 list-disc"><a href=""></a>Class 1</li>
+                    <li className="p-5 list-disc"><a href=""></a>Class 1</li>
+                    <li className="p-5 list-disc"><a href=""></a>Class 1</li>
+                    <li className="p-5 list-disc"><a href=""></a>Class 1</li>
+                </ul>
+            </div>
+        </div>
         </>
      );
 }
