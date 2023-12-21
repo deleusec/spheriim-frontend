f65da913dd308d256611d7c61b4869dd9f775370
Corentin Deleuse <dlse.corentin@gmail.com>
2023-12-21 11:26:57 +0100

    update class page

--- src/pages/Class.tsx
+++ src/pages/Class.tsx
@@ -5,18 +5,16 @@ import Students from '../pages/Students';
 function Class() {
     return (
         <>
-            <ClassInfo/>
-            <div className="grid grid-cols-9 auto-rows-auto gap-5 px-5">
+            <ClassInfo />
+            <div className="grid grid-cols-9 auto-rows-auto gap-8 px-8">
                 <div className="flex col-start-1 col-span-4 row-start-1 row-span-1 h-full w-full">
-                    <NextLesson/>
+                    <NextLesson />
                 </div>
                 <div className="flex col-start-5 col-span-6 row-start-1 row-span-1 h-full w-full">
-                    <StudentBio bio="Souhaite devenir développeuse Front-end ou chef de projet et travailler à l’étranger."/>
+                    <StudentBio bio="Souhaite devenir développeuse Front-end ou chef de projet et travailler à l’étranger." />
                 </div>
             </div>
-            <div className="m-5">
-                <Students/>
-            </div>
+            <Students />
         </>
     );
 }
