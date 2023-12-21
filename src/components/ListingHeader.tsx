8ccb6cd88e3c110bfa438451034b3aba56cbb129
Corentin Deleuse <dlse.corentin@gmail.com>
2023-12-21 09:14:04 +0100

    undo font bold

--- src/components/ListingHeader.tsx
+++ src/components/ListingHeader.tsx
@@ -7,7 +7,7 @@ interface ListingHeaderProps {
 function ListingHeader(props: ListingHeaderProps) {
     return ( 
         <div className="w-full p-8">
-            <h1 className="font-semibold">{props.title}</h1>
+            <h1>{props.title}</h1>
             {props.subtitle && <h2 className="text-primary font-semibold mt-1">{props.subtitle}</h2>}
             {props.description && <p className="text-sm text-dark mt-4 max-w-[500px]">{props.description}</p>}
         </div>
