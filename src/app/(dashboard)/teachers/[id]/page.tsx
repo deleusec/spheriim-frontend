436a9ea30c09d75b44a2164b271a50cfe1b23805
Corentin Deleuse <dlse.corentin@gmail.com>
2024-02-13 10:26:08 +0100

    feat: teacher single page

--- src/app/(dashboard)/teachers/[id]/page.tsx
+++ src/app/(dashboard)/teachers/[id]/page.tsx
@@ -1,4 +1,10 @@
-export default function Teacher() {
+interface TeacherProps {
+    params: { id: string };
+}
+
+export default function Teacher({ params } : TeacherProps ) {
+    console.log(params.id);
+    
     return (
         <h1>Hello World</h1>
     );
