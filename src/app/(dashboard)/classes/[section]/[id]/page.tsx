40a7f37ff14a12bb5c70c5b0129bafc7c9e36332
walltch <walltch@github.com>
2024-02-14 14:29:00 +0100

    class 1

---
+++ src/app/(dashboard)/classes/[section]/[id]/page.tsx
@@ -0,0 +1,38 @@
+'use client';
+import ClassInfo from "@/components/ClassInfo";
+import NextLesson from "@/components/NextLesson";
+// import Students from "@/components/Students";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+import { data } from "@/components/Graph";
+// import Calendar from "@/components/Calendar";
+
+const NewStudents = data.datasets[0].data[1]
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export default function Class() {
+    return (
+        <>
+            <ClassInfo/>
+            <div className="grid grid-cols-10 auto-rows-auto gap-5 px-8">
+                <div className="flex col-start-1 col-span-5 row-start-1 row-span-1 h-full w-full">
+                    <NextLesson/>
+                </div>
+                <div className="flex col-start-6 col-span-5 row-start-1 row-span-1 h-72 w-full justify-center rounded-xl shadow-cards bg-white p-5 relative">
+                    <Doughnut data={data}/>
+                    <p className="absolute text-4xl text-primary" style={{ top: '47%' }}>{NewStudents}%</p>
+                </div>
+            </div>
+            <div className="mt-10 rounded-xl shadow-cards bg-white mx-8">
+                {/* <Students/> */}
+            </div>
+            <div className="mt-10 rounded-xl shadow-cards bg-white mx-8">
+                {/* <Calendar/> */}
+            </div>
+
+        </>
+    );
+}
+
+
---
+++ src/components/Graph.tsx
@@ -0,0 +1,26 @@
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+
+export const data = {
+  type: 'doughnut',
+  labels: ['Anciens', 'Nouveaux'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [55, 45],
+      layout: 20,
+      backgroundColor: [
+        'rgba(240, 125, 0, 0.2)',
+        'rgba(240, 125, 0)',
+      ],
+      borderWidth: 0, 
+    },
+  ],
+};
+
+export function Graph() {
+  return <Doughnut data={data}/>;
+}
